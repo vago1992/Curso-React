1ccc1fb07fd357c46c7373a5441626f759d405e8
Santiago Leonardis <santiagoleonardis@gmail.com>
2022-04-20 20:30:51 -0300

    2do commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-import Saludo from './Components/Saludos';
-import ConHijos from './Components/ConHijos';
 import NavBar from './Components/NavBar';
+import ItemListContainer from './Components/ItemListContainer';
 
 function App() {
   return (
-    /*<div className="App">
-      <ConHijos>
-        <Saludo name="Marcelo" age="35"/>
-      </ConHijos>
-      <h1 style={{color:"red"}}>Hola Mundo</h1>
 
-      <Saludo name="Juan" age="32"/>
-    </div>*/
     <div>
-      <NavBar/>
+      <ItemListContainer>
+        <NavBar/>
+      </ItemListContainer>
+      
     </div>
   );
 }
---
+++ src/Components/ItemListContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ItemListContainer = (props) => {
+  return (
+      <>
+        {props.children}
+      </>
+  )
+}
+
+export default ItemListContainer
